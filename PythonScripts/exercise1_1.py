d90d19efc10f69b4c9d338b77b71d65bec5aea72
AnnaElo <anna_eloranta@outlook.com>
2025-11-19 19:31:32 +0200

    Iteration example in Python.

--- PythonScripts/exercise1_1.py
+++ PythonScripts/exercise1_1.py
@@ -1,1 +1,21 @@
-# First exercise of the course
+# Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
+# Iterator that returns numbers from 1 to 20 (https://www.w3schools.com/python/python_iterators.asp)
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
