ef59d4f4bdfda5c5f7f01d36fad96d01403b39f5
AnnaElo <anna_eloranta@outlook.com>
2025-12-04 20:32:01 +0200

    Updated settings & code

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -4,11 +4,19 @@
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt 
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Grey-scaling the image
+    blur = cv2.GaussianBlur(gray, (5, 5), 0) # Optional method - Blurring the image to reduce noise
+    canny = cv2.Canny(blur, 50, 150) # Canny method to detect edges in the image with 1:3 ratio
+    return canny
 
 # Program to read the image, grey-scale and display it
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image) # Working inside a copy of the image, not to affect the original one
-gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY) # Grey-scaling the image
+canny = canny(lane_image)
 
-cv2.imshow('result', gray)
-cv2.waitKey(0) # Time of the display, zero is infinite
+plt.imshow(canny)
+plt.show()
+#cv2.waitKey(0) # Time of the display, zero is infinite
